d6cc23b5b447bdb316ed9aa69bec3e3d73f5ff92
youssefAshraf21 <ya614962@gmail.com>
2026-09-21 14:59:56 +0300

    Add HTML structure and JavaScript functionality for personalized greeting and currency converter

---
+++ javascript1/main.js
@@ -0,0 +1,21 @@
+
+// 1. Personalized Greeting
+
+/*   var userName = prompt("Enter Your Name");
+var age = prompt("Enter Your Age");
+var dateOfBirth = 2026 - age;
+console.log("Hello " + userName + "!" + " You are " + age + " years old " + "and you were born around " + dateOfBirth);
+*/
+
+
+//2. Currency Converter
+
+var hour ;
+if (hour <= 11) {
+console.log("Good morning!")
+}
+else if (hour >= 12 && hour <= 17) {
+console.log("Good afternoon!")
+}else {
+console.log("Good evening!")
+}
